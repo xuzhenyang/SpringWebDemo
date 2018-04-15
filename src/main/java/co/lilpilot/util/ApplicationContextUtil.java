720008b91843ab0ef5dc6732e941b369efacadda
徐祯阳 <xuzhenyang@souche.com>
2018-04-15 21:09:31 +0800

    add ApplicationContextUtil

---
+++ src/main/java/co/lilpilot/util/ApplicationContextUtil.java
@@ -0,0 +1,22 @@
+package co.lilpilot.util;
+
+import lombok.Getter;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by lilpilot on 15/04/2018.
+ */
+@Component
+public class ApplicationContextUtil implements ApplicationContextAware {
+
+    @Getter
+    private static ApplicationContext context;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+}
